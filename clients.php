d11acf10af9eb221d0374f88dee01b2e6b84c73b
Nelson Sammy <psammy.ngei@gmail.com>
2022-06-26 19:23:45 +0300

    updated cliets

--- clients.php
+++ clients.php
@@ -59,8 +59,80 @@ if (isset($_POST['save'])){
                                                 <td><?php echo $row['email']; ?></td>
                                                 <td><?php echo $row['phone']; ?></td>
                                                 <td><?php echo $row['gender']; ?></td>
-                                                <td></td>
+                                                <td>
+                                                    <div class="dropdown mb-4">
+                                                        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                            Action
+                                                        </button>
+                                                        <div class="dropdown-menu animated--fade-in" aria-labelledby="dropdownMenuButton" style="">
+                                                            <a class="dropdown-item" href="clients/view.php">View</a>
+                                                            <a class="dropdown-item" data-toggle="modal" data-target="#editClient<?php echo $row['id']?>">Edit</a>
+                                                            <a class="dropdown-item" data-toggle="modal" data-target="#deleteClient<?php echo $row['id']?>">Delete</a>
+                                                        </div>
+                                                    </div>
+                                                </td>
                                             </tr>
+                                            <div class="modal fade" id="deleteClient<?php echo $row['id']?>">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <form action="clients/delete.php" method="post">
+                                                            <div class="modal-body">
+                                                                <center>
+                                                                    <i class="fa fa-trash fa-5x" style="color: red"></i>
+                                                                    <p>Are You sure you want to delete <?php echo $row['fullname']?></p>
+                                                                </center>
+                                                            </div>
+                                                            <div class="modal-footer justify-content-center">
+                                                                <button class="btn btn-sm btn-warning" type="button" data-dismiss="modal">
+                                                                    Not Now
+                                                                </button>
+                                                                <button class="btn btn-sm btn-danger" type="submit" name="delete">
+                                                                    Delete Client
+                                                                </button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="modal fade" id="editClient<?php echo $row['id']?>">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <form method="POST" action="clients/edit.php">
+                                                            <div class="modal-body">
+                                                                <input type="hidden" name="id" value="<?php echo $row['id']?>">
+                                                                <div class="form-group">
+                                                                    <label for="name">Client Name</label>
+                                                                    <input id="name" type="text" name="name" class="form-control" value="<?php echo $row['fullname']?>">
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label for="email">Client Email</label>
+                                                                    <input id="email" type="email" name="email" class="form-control" value="<?php echo $row['email']?>">
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label for="phone">Client Phone Number</label>
+                                                                    <input id="phone" type="text" name="phone" class="form-control" value="<?php echo $row['phone']?>">
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label for="gender">Gender</label>
+                                                                    <select name="gender" id="gender" class="form-control">
+                                                                        <option disabled>Select Gender</option>
+                                                                        <option value="Female">Female</option>
+                                                                        <option value="Male">Male</option>
+                                                                    </select>
+                                                                </div>
+                                                            </div>
+                                                            <div class="modal-footer justify-content-center">
+                                                                <button class="btn btn-sm btn-warning" type="button" data-dismiss="modal">
+                                                                    Not Now
+                                                                </button>
+                                                                <button class="btn btn-sm btn-success" type="submit" name="edit">
+                                                                    Update Client
+                                                                </button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
                                     <?php
                                         }
                                     ?>
